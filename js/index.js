db05f22d2110c57c0c50d68b1fa9e5ae757d6782
Sammy Uyghur <miever1@163.com>
2020-01-03 17:01:32 +0800

    function is done

--- js/index.js
+++ js/index.js
@@ -20,13 +20,17 @@ let flag = true;
 for (let i = 0; i < 9; i++) {
     $(".sqare")[i].addEventListener("click", function () {
         if (flag === true) {
-            //$(".sqare")[i].className = "sqare icon-radio-unchecked";
+            $(".sqare")[i].className = "sqare icon-radio-unchecked";
             $(".sqare")[i].innerText = 1;
+            $("#player2").style.color = "black";
+            $("#player1").style.color = "red";
             flag = false;
         }
         else {
-            //$(".sqare")[i].className = "sqare icon-star-full";
+            $(".sqare")[i].className = "sqare icon-cross";
             $(".sqare")[i].innerText = 2;
+            $("#player1").style.color = "black";
+            $("#player2").style.color = "red";
             flag = true;
         }
         $(".sqare")[i].disabled = "true";
@@ -34,30 +38,33 @@ for (let i = 0; i < 9; i++) {
 }
 
 
-let board = [8, 8, 8, 8, 8, 8, 8, 8, 8];
+let board = ["", "", "", "", "", "", "", "", ""];
 let winBoard = [false, false, false, false, false, false, false, false];
-setInterval(() => {
+let timer = setInterval(() => {
     for (let i = 0; i < 9; i++) {
         board[i] = Number($(".sqare")[i].innerText);
     }
-    let a = checkItem();
-    if (a == true) {
-        alert("yes");
-    };
-}, 1000);
+    checkItem();
+}, 300);
 
 function checkItem() {
-    winBoard[0] = (board[0] == board[1] && board[1] == board[2] && board[0] != 8);
-    winBoard[1] = (board[3] == board[4] && board[4] == board[5] && board[3] != 8);
-    winBoard[2] = (board[6] == board[7] && board[7] == board[8] && board[6] != 8);
-    winBoard[3] = (board[0] == board[3] && board[3] == board[6] && board[0] != 8);
-    winBoard[4] = (board[1] == board[4] && board[4] == board[7] && board[1] != 8);
-    winBoard[5] = (board[2] == board[5] && board[5] == board[8] && board[2] != 8);
-    winBoard[6] = (board[0] == board[4] && board[4] == board[8] && board[0] != 8);
-    winBoard[7] = (board[2] == board[4] && board[4] == board[6] && board[2] != 8);
+    winBoard[0] = (board[0] == board[1] && board[1] == board[2] && board[0] != "");
+    winBoard[1] = (board[3] == board[4] && board[4] == board[5] && board[3] != "");
+    winBoard[2] = (board[6] == board[7] && board[7] == board[8] && board[6] != "");
+    winBoard[3] = (board[0] == board[3] && board[3] == board[6] && board[0] != "");
+    winBoard[4] = (board[1] == board[4] && board[4] == board[7] && board[1] != "");
+    winBoard[5] = (board[2] == board[5] && board[5] == board[8] && board[2] != "");
+    winBoard[6] = (board[0] == board[4] && board[4] == board[8] && board[0] != "");
+    winBoard[7] = (board[2] == board[4] && board[4] == board[6] && board[2] != "");
     for (let i = 0; i < 8; i++) {
         if (winBoard[i] == true) {
-            return true;
+            if (flag === true) {
+                alert("player " + 2 + " win");
+            }
+            else {
+                alert("player " + 1 + " win");
+            }
+            clearInterval(timer);
         }
     }
 }
